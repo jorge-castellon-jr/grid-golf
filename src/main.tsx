a81d98d040f639c099691a5cdb8b55b1b02162b6
Jorge Castellon Jr <jorge.castellon.jr@gmail.com>
2025-02-26 19:27:47 -0800

    comments the service worker for now

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "./registerSW.ts";
+// import "./registerSW.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
